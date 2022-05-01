4a3b8e735ee51067f1467a5fe5fc509301c7cf81
erxclau <ericlau41802@gmail.com>
2022-05-01 11:39:11 -0400

    Initialize vue app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/preview">Preview</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
