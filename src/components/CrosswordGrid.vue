165ada2cfe6722b7badbd5b062227cb7bed1a510
erxclau <ericlau41802@gmail.com>
2022-05-01 17:27:17 -0400

    Refactor code

--- src/components/CrosswordGrid.vue
+++ src/components/CrosswordGrid.vue
@@ -1,137 +1,130 @@
 <script>
 const EMPTY = "/";
+const setColor = (element, color) => {
+  element.style.backgroundColor = color;
+};
+
+const normalize = (num) => {
+  if (num < 0) {
+    num = 0;
+  }
+  if (num > 14) {
+    num = 14;
+  }
+  return num;
+};
 
 export default {
   data() {
     return {
       grid: new Array(15 * 15).fill(" "),
       vertical: false,
-      activeIndex: null,
+      active: {
+        i: null,
+        row: null,
+        col: null,
+        el: null,
+      },
       lastBack: false,
     };
   },
   methods: {
-    fill(v, i) {
+    setItem(v, i) {
       this.grid[i] = v;
       const storage = JSON.stringify(this.grid);
       localStorage.setItem("grid", storage);
     },
-    colorBlack(e, i) {
-      const row = Math.floor(i / 15);
-      const col = i % 15;
+    colorUpdate(e) {
+      const { row, col, i } = this.active;
       const orow = Math.abs(15 - row) - 1;
       const ocol = Math.abs(15 - col) - 1;
 
       const item = this.$refs.items[i];
       if (e.target.value === EMPTY) {
-        item.style.backgroundColor = "black";
-        this.$refs.items[orow * 15 + ocol].style.backgroundColor = "black";
-        this.fill(EMPTY, orow * 15 + ocol);
+        setColor(item, "black");
+        setColor(this.$refs.items[orow * 15 + ocol], "black");
+        this.setItem(EMPTY, orow * 15 + ocol);
       } else {
         if (item.style.backgroundColor === "black") {
-          item.style.backgroundColor = "white";
+          setColor(item, "white");
         }
       }
 
       if (this.grid[i] === EMPTY && e.target.value !== EMPTY) {
-        item.style.backgroundColor = "lightcoral";
-        this.$refs.items[orow * 15 + ocol].style.backgroundColor = "white";
-        this.fill(" ", orow * 15 + ocol);
+        setColor(item, "lightcoral");
+        setColor(this.$refs.items[orow * 15 + ocol], "white");
+        this.setItem(" ", orow * 15 + ocol);
       }
     },
-    colorRow(row) {
-      const selected = document.activeElement;
-      for (let i = 0; i < 15; i++) {
-        const index = row * 15 + i;
-        const item = this.$refs.items[index];
-        if (item !== selected && this.grid[index] !== EMPTY) {
-          item.style.backgroundColor = "lightyellow";
-        }
-      }
-    },
-    colorColumn(col) {
-      const selected = document.activeElement;
-      for (let i = 0; i < 15; i++) {
-        const index = i * 15 + col;
-        const item = this.$refs.items[index];
-        if (item !== selected && this.grid[index] !== EMPTY) {
-          item.style.backgroundColor = "lightyellow";
-        }
-      }
-    },
-    colorFocus(i) {
-      const row = Math.floor(i / 15);
-      const col = i % 15;
+    update(e) {
+      let { row, col, i } = this.active;
 
+      this.colorUpdate(e);
+      this.setItem(e.target.value.toUpperCase(), i);
+
+      if (this.vertical) {
+        row = normalize(row + (this.lastBack ? -1 : 1));
+      } else {
+        col = normalize(col + (this.lastBack ? -1 : 1));
+      }
+
+      this.moveActive(e, row * 15 + col);
+    },
+    colorFocus() {
       this.$refs.items.forEach((item) => {
         if (
           item.style.backgroundColor === "lightcoral" ||
           item.style.backgroundColor === "lightyellow"
         ) {
-          item.style.backgroundColor = "white";
+          setColor(item, "white");
         }
         if (item === document.activeElement) {
-          if (this.grid[i] === EMPTY) {
-            item.style.backgroundColor = "black";
+          if (this.grid[this.active.i] === EMPTY) {
+            setColor(item, "black");
           } else {
-            item.style.backgroundColor = "lightcoral";
+            setColor(item, "lightcoral");
           }
         }
       });
 
-      if (this.vertical) {
-        this.colorColumn(col);
-      } else {
-        this.colorRow(row);
-      }
-    },
-    colorMask() {
-      this.$refs.items.forEach((item, i) => {
-        if (this.grid[i] === EMPTY) {
-          item.style.backgroundColor = "black";
-        }
-      });
-    },
-    update(e, i) {
-      this.colorBlack(e, i);
-      this.fill(String(e.target.value).toUpperCase(), i);
+      const getIndex = this.vertical
+        ? (i) => i * 15 + this.active.col
+        : (i) => this.active.row * 15 + i;
 
-      let row = Math.floor(this.activeIndex / 15);
-      let col = this.activeIndex % 15;
-      if (this.vertical) {
-        row += this.lastBack ? -1 : 1;
-        if (row < 0) {
-          row = 0;
-        }
-        if (row > 14) {
-          row = 14;
-        }
-      } else {
-        col += this.lastBack ? -1 : 1;
-        if (col < 0) {
-          col = 0;
-        }
-        if (col > 14) {
-          col = 14;
+      for (let i = 0; i < 15; i++) {
+        const index = getIndex(i);
+        const item = this.$refs.items[index];
+        if (item !== this.active.el && this.grid[index] !== EMPTY) {
+          setColor(item, "lightyellow");
         }
       }
-
-      this.moveActive(e, row * 15 + col);
     },
     focus(e, i) {
       e.target.select();
-      this.activeIndex = i;
-      this.colorFocus(i);
+
+      this.active.i = i;
+      this.active.row = Math.floor(i / 15);
+      this.active.col = i % 15;
+      this.active.el = document.activeElement;
+
+      this.colorFocus();
     },
-    moveActive(e, index) {
-      if (index !== this.activeIndex) {
+    moveActive(e, i) {
+      if (i !== this.active.i) {
         e.preventDefault();
-        this.activeIndex = index;
-        const item = this.$refs.items[index];
+        this.active.i = i;
+        const item = this.$refs.items[i];
         item.focus();
         item.select();
       }
-      this.colorFocus(this.activeIndex);
+      this.colorFocus();
+    },
+    colorMount() {
+      this.$refs.items.forEach((item, i) => {
+        if (this.grid[i] === EMPTY) {
+          setColor(item, "black");
+        }
+      });
     },
     navigate(e) {
       this.lastBack = e.key === "Backspace";
@@ -143,34 +136,20 @@ export default {
       ]);
       const selected = document.querySelector(".item:focus-visible");
       if (selected && arrows.has(e.key)) {
-        let row = Math.floor(this.activeIndex / 15);
-        let col = this.activeIndex % 15;
+        let { row, col } = this.active;
         if (this.vertical) {
           if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
             this.vertical = false;
           } else {
-            row += e.key === "ArrowUp" ? -1 : 1;
-            if (row < 0) {
-              row = 0;
-            }
-            if (row > 14) {
-              row = 14;
-            }
+            row = normalize(row + (e.key === "ArrowUp" ? -1 : 1));
           }
         } else {
           if (e.key === "ArrowUp" || e.key === "ArrowDown") {
             this.vertical = true;
           } else {
-            col += e.key === "ArrowLeft" ? -1 : 1;
-            if (col < 0) {
-              col = 0;
-            }
-            if (col > 14) {
-              col = 14;
-            }
+            col = normalize(col + (e.key === "ArrowLeft" ? -1 : 1));
           }
         }
-
         this.moveActive(e, row * 15 + col);
       }
     },
@@ -180,7 +159,7 @@ export default {
     const storage = localStorage.getItem("grid");
     if (storage) {
       this.grid = JSON.parse(storage);
-      this.colorMask();
+      this.colorMount();
     }
   },
   beforeUnmount() {
@@ -195,7 +174,6 @@ export default {
     <section class="grid">
       <input
         class="item"
-        pattern="[a-zA-Z /]"
         maxlength="1"
         ref="items"
         v-for="(_, i) in grid"
@@ -203,7 +181,7 @@ export default {
         :value="grid[i]"
         @click="(e) => e.target.select()"
         @focus="(e) => focus(e, i)"
-        @input="(e) => update(e, i)"
+        @input="update"
       />
     </section>
   </article>
