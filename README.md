4804cc87d5f082bed44387c6cb9d11f644c6d0d1
erxclau <ericlau41802@gmail.com>
2022-05-05 23:09:25 -0400

    Rewrite in Svelte

--- README.md
+++ README.md
@@ -4,33 +4,21 @@
 
 ---
 
-# svelte app
-
-This is a project template for [Svelte](https://svelte.dev) apps. It lives at https://github.com/sveltejs/template.
-
-To create a new project based on this template using [degit](https://github.com/Rich-Harris/degit):
-
-```bash
-npx degit sveltejs/template svelte-app
-cd svelte-app
-```
-
-*Note that you will need to have [Node.js](https://nodejs.org) installed.*
-
+# crossword
 
 ## Get started
 
 Install the dependencies...
 
 ```bash
-cd svelte-app
-npm install
+cd crossword
+yarn install
 ```
 
 ...then start [Rollup](https://rollupjs.org):
 
 ```bash
-npm run dev
+yarn run dev
 ```
 
 Navigate to [localhost:8080](http://localhost:8080). You should see your app running. Edit a component file in `src`, save it, and reload the page to see your changes.
@@ -44,11 +32,10 @@ If you're using [Visual Studio Code](https://code.visualstudio.com/) we recommen
 To create an optimised version of the app:
 
 ```bash
-npm run build
+yarn run build
 ```
 
-You can run the newly built app with `npm run start`. This uses [sirv](https://github.com/lukeed/sirv), which is included in your package.json's `dependencies` so that the app will work when you deploy to platforms like [Heroku](https://heroku.com).
-
+You can run the newly built app with `yarn run start`. This uses [sirv](https://github.com/lukeed/sirv), which is included in your package.json's `dependencies` so that the app will work when you deploy to platforms like [Heroku](https://heroku.com).
 
 ## Single-page app mode
 
@@ -60,22 +47,6 @@ If you're building a single-page app (SPA) with multiple routes, sirv needs to b
 "start": "sirv public --single"
 ```
 
-## Using TypeScript
-
-This template comes with a script to set up a TypeScript development environment, you can run it immediately after cloning the template with:
-
-```bash
-node scripts/setupTypeScript.js
-```
-
-Or remove the script via:
-
-```bash
-rm scripts/setupTypeScript.js
-```
-
-If you want to use `baseUrl` or `path` aliases within your `tsconfig`, you need to set up `@rollup/plugin-alias` to tell Rollup to resolve the aliases. For more info, see [this StackOverflow question](https://stackoverflow.com/questions/63427935/setup-tsconfig-path-in-svelte).
-
 ## Deploying to the web
 
 ### With [Vercel](https://vercel.com)
@@ -83,14 +54,14 @@ If you want to use `baseUrl` or `path` aliases within your `tsconfig`, you need 
 Install `vercel` if you haven't already:
 
 ```bash
-npm install -g vercel
+yarn install -g vercel
 ```
 
 Then, from within your project folder:
 
 ```bash
 cd public
-vercel deploy --name my-project
+vercel deploy --name crossword
 ```
 
 ### With [surge](https://surge.sh/)
@@ -98,12 +69,12 @@ vercel deploy --name my-project
 Install `surge` if you haven't already:
 
 ```bash
-npm install -g surge
+yarn install -g surge
 ```
 
 Then, from within your project folder:
 
 ```bash
-npm run build
-surge public my-project.surge.sh
+yarn run build
+surge public crossword.surge.sh
 ```
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,262 @@
 <script>
-	export let name;
+  import { onMount } from "svelte";
+
+  const size = 15;
+  const empty = " ";
+  const block = "/";
+  let grid = Array();
+  let labels = Array();
+  let vertical = false;
+
+  let current;
+
+  $: grid.length && localStorage.setItem("grid", JSON.stringify(grid));
+  $: (() => {
+    labels = grid.reduce((arr, sq, i) => {
+      const { row, col } = coordinate(i);
+      if (sq !== block) {
+        const top = grid[(row - 1) * 15 + col];
+        const left = grid[row * 15 + col - 1];
+
+        if (row === 0 || col === 0) {
+          arr.push({ index: i, text: arr.length + 1 });
+        } else if (top === block || left === block) {
+          arr.push({ index: i, text: arr.length + 1 });
+        }
+      }
+      return arr;
+    }, Array());
+  })();
+
+  const row = (i) => Math.floor(i / size);
+  const col = (i) => i % size;
+  const coordinate = (i) => ({ row: Math.floor(i / size), col: i % size });
+  const normalize = (major, minor, increment) => {
+    const tmp = major + increment;
+    if (tmp < 0 && minor > 0) {
+      minor--;
+      major = size - 1;
+    } else if (tmp > size - 1 && minor < size - 1) {
+      minor++;
+      major = 0;
+    } else if (tmp < size && tmp > -1) {
+      major = tmp;
+    }
+    return { major, minor };
+  };
+
+  const next = (i, forward) => {
+    let { row, col } = coordinate(i);
+    const v = forward ? 1 : -1;
+    if (vertical) {
+      ({ major: row, minor: col } = normalize(row, col, v));
+    } else {
+      ({ major: col, minor: row } = normalize(col, row, v));
+    }
+    return row * size + col;
+  };
+
+  const move = (selection, i) => {
+    const child = selection.parentNode.children[i];
+    child.focus();
+    child.select();
+  };
+
+  const focus = (e, i) => {
+    const square = e.target;
+    square.select();
+    current = i;
+  };
+
+  const clear = () => {
+    grid = Array(size * size).fill(empty);
+  };
+
+  const reflect = (i) => {
+    const { row, col } = coordinate(i);
+    const orow = Math.abs(size - row) - 1;
+    const ocol = Math.abs(size - col) - 1;
+    return orow * size + ocol;
+  };
+
+  onMount(() => {
+    const storage = localStorage.getItem("grid");
+    grid = storage ? JSON.parse(storage) : Array(size * size).fill(empty);
+  });
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<svelte:window
+  on:keydown={(e) => {
+    const arrows = new Set(["ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"]);
+    const selected = document.querySelector(".square:focus-visible");
+    if (selected && arrows.has(e.key)) {
+      e.preventDefault();
+
+      const index = [...selected.parentNode.children].indexOf(selected);
+      let n = index;
+
+      if (vertical) {
+        if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+          vertical = false;
+        } else {
+          n = next(index, e.key === "ArrowDown");
+        }
+      } else {
+        if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+          vertical = true;
+        } else {
+          n = next(index, e.key === "ArrowRight");
+        }
+      }
+
+      move(selected, n);
+    }
+  }}
+/>
+
+<main style="--size: {size};">
+  <h2>Crossword</h2>
+  <details>
+    <summary><b>Guidelines</b></summary>
+    <ol>
+      <li>Do not have more than 38 black squares</li>
+      <li>Do not have more than 78 words</li>
+      <li>Do not have 1 or 2 letter words</li>
+      <li>The puzzle theme must consist of at least 3 entries</li>
+      <li>The puzzle theme must total at least 33 squares</li>
+    </ol>
+  </details>
+  <button class="clear" on:click={clear}>Clear</button>
+  <section id="grid">
+    {#each grid as square, i}
+      <input
+        id="square-{i}"
+        class="square"
+        class:block={square === block}
+        class:active={i === current}
+        class:axis={(() => {
+          const axis = vertical ? "col" : "row";
+          return (
+            coordinate(i)[axis] === coordinate(current)[axis] &&
+            i !== current &&
+            square !== block
+          );
+        })()}
+        maxlength="1"
+        on:click={(e) => e.target.select()}
+        on:focus={(e) => focus(e, i)}
+        on:input={(e) => {
+          const input = e.target;
+          const v = input.value.toUpperCase();
+
+          if (square !== block && v === block) {
+            grid[reflect(i)] = block;
+          }
+          if (square === block && v !== block) {
+            grid[reflect(i)] = empty;
+          }
+
+          const insert = e.inputType === "insertText";
+          grid[i] = insert ? v : empty;
+
+          const n = next(i, insert);
+          if (i === n && grid[i] === empty) {
+            grid[i] = "";
+          }
+          move(e.target, n);
+        }}
+        bind:value={square}
+      />
+    {/each}
+    {#each labels as label}
+      <label
+        class="label"
+        style:top="calc({row(label.index)} * var(--squarepx) + 1.5px)"
+        style:left="calc({col(label.index)} * var(--squarepx) + 1.5px)"
+        for="square-{label.index}"
+        >{label.text}
+      </label>
+    {/each}
+  </section>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :root {
+    --squarepx: 32.5px;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  main {
+    margin: 0.5rem;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  details {
+    margin: 1rem 0;
+  }
+
+  ol {
+    max-width: calc(var(--size) * var(--squarepx));
+    padding-inline-start: 20px;
+    margin: 0.5rem 0;
+  }
+
+  .clear {
+    margin-bottom: 0.5rem;
+    padding: 0.25rem 0.5rem;
+    background-color: rgb(211, 222, 226);
+    border: none;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  .clear:hover {
+    background-color: rgb(165, 172, 175);
+  }
+
+  #grid {
+    display: grid;
+    grid-template-columns: repeat(var(--size), var(--squarepx));
+    position: relative;
+    width: fit-content;
+    outline: 1px solid black;
+    box-sizing: border-box;
+    margin: 0px;
+  }
+
+  .square {
+    width: var(--squarepx);
+    height: var(--squarepx);
+
+    display: grid;
+    align-items: center;
+    justify-content: center;
+
+    font-weight: bold;
+    text-align: center;
+    text-transform: uppercase;
+
+    background-color: white;
+    padding: 0;
+    outline: 1px solid black;
+    border: unset;
+  }
+
+  .square::selection {
+    background: transparent;
+  }
+
+  .block {
+    background-color: black;
+  }
+
+  .active {
+    background-color: lightcoral;
+  }
+
+  .axis {
+    background-color: lightyellow;
+  }
+
+  .label {
+    position: absolute;
+    font-size: 9px;
+  }
 </style>
